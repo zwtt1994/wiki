2bab94492ba222dccbdf5ac1af161aa55c4584ff
zhangweite <zhangweite@meituan.com>
2019-10-19 10:41:38 +0800

    init-version

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+if [ "$1" = "-i" ]
+then
+    mkdir output
+    cd output
+    git clone -b gh-pages git@github.com:zwtt1994/wiki.git ./
+    cd ..
+    exit 0
+elif [ "$1" = "" ]
+then
+    echo deploy [Option]
+    echo "       -i 初始化"
+    echo "       message  提交到github并发布，提交信息为mesage"
+    exit 0
+else
+    git add . --all
+    git commit -am "$1"
+    git pull origin master
+    git push origin master
+
+    simiki g
+    cd output
+    mkdir src
+    cp ../src/*.html src/
+    cp ../src/*/*.html src/
+    git add . --all
+    git commit -am "$1"
+    git pull origin gh-pages
+    git push origin gh-pages
+    cd ..
+    
+    
+
+
+fi
