1ad93fc950eb55de9107adb8b641329b7ebf06e9
zhangweite <zhangweite@meituan.com>
2019-10-21 19:05:33 +0800

    de

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,17 @@
-# added by Anaconda3 2018.12 installer
-# >>> conda init >>>
+# >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$(CONDA_REPORT_ERRORS=false '/Users/zhangweite/anaconda3/bin/conda' shell.bash hook 2> /dev/null)"
+__conda_setup="$('/Users/zhangweite/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
-    \eval "$__conda_setup"
+    eval "$__conda_setup"
 else
     if [ -f "/Users/zhangweite/anaconda3/etc/profile.d/conda.sh" ]; then
-# . "/Users/zhangweite/anaconda3/etc/profile.d/conda.sh"  # commented out by conda initialize
-        CONDA_CHANGEPS1=false conda activate base
+        . "/Users/zhangweite/anaconda3/etc/profile.d/conda.sh"
     else
-        \export PATH="/Users/zhangweite/anaconda3/bin:$PATH"
+        export PATH="/Users/zhangweite/anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
-# <<< conda init <<<
+# <<< conda initialize <<<
 
 
 if [ "$1" = "-i" ]
